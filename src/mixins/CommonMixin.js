71abed016b1fc3c1f0889e44505d550aae6061e2
ManojPandiS <35683532+ManojPandiS@users.noreply.github.com>
2022-06-23 18:29:46 +0530

    mixin method changes

--- src/mixins/CommonMixin.js
+++ src/mixins/CommonMixin.js
@@ -5,28 +5,27 @@ export const CommonMixin = {
         }
     },
 
-    computed: {
-        getFormatedTime() {
-            return date => {
-                let seconds = Math.floor((new Date().getTime() - date) / 1000);
-        
-                let interval = seconds / 86400;
-                if (interval > 1) {
-                    if (interval > 365) {
-                        return Math.floor(interval / 365) + " Years ago";
-                    }
-                    if (interval > 30) {
-                        return Math.floor(interval / 30) + " Months ago";
-                    }
-                    return Math.floor(interval) + " days ago";
+    methods: {
+        getFormatedTime( date ) {
+            let seconds = Math.floor((new Date().getTime() - date) / 1000);
+    
+            let interval = seconds / 86400;
+            if (interval > 1) {
+                if (interval > 365) {
+                    return Math.floor(interval / 365) + " Years ago";
                 }
-                return "Just now";
-            } 
+                if (interval > 30) {
+                    return Math.floor(interval / 30) + " Months ago";
+                }
+                return Math.floor(interval) + " days ago";
+            }
+            return "Just now";
         },
         
-        getPhotoURL() {
-            return url => 'https://message-list.appspot.com' + url;
+        getPhotoURL( url ) {
+            return 'https://message-list.appspot.com' + url;
         }
     }
 }
 
+export default 'defaulttext';
